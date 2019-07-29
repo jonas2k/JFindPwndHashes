a63d374598dc7f0fb357e306b96942a248990199
jonas2k <jonas2k@users.noreply.github.com>
2019-07-29 10:52:00 +0200

    Some minor corrections

---
+++ src/main/java/com/anconet/JFindPwndHashes/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.anconet.JFindPwndHashes.utils;
+
+public class Constants {
+	public static final String AUTHOR = "jonas2k";
+	public static final String pomPackagePath = "/META-INF/maven/com.anconet/JFindPwndHashes/pom.xml";
+}
