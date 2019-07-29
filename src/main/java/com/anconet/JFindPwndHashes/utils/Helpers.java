25053767626605f16a51b5672cebd83bd34c7693
jonas2k <jonas2k@users.noreply.github.com>
2019-07-29 09:00:38 +0200

    Some cosmetics

--- src/main/java/com/anconet/JFindPwndHashes/utils/Helpers.java
+++ src/main/java/com/anconet/JFindPwndHashes/utils/Helpers.java
@@ -1,6 +1,7 @@
 package com.anconet.JFindPwndHashes.utils;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -10,9 +11,12 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
 public class Helpers {
-	
+
 	public static List<String> readFile(String fileName) {
 		try {
 			return FileUtils.readLines(new File(fileName), StandardCharsets.UTF_8);
@@ -29,5 +33,25 @@ public class Helpers {
 			e.printStackTrace();
 		}
 		return Stream.of(new String[] { "" });
+	}
+
+	public static long getLineCount(String fileName) {
+
+		long lineCount = 0;
+		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+			lineCount = stream.count();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return lineCount;
+	}
+	
+	public static Model getMavenModel() {
+        try {
+			return new MavenXpp3Reader().read(new FileReader("pom.xml"));
+		} catch (IOException | XmlPullParserException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 }
